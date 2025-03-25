663124c72fff1cee7e2df3980d874fd91202603a
Dolores Derksen <doloresderksen@gmail.com>
2025-03-25 12:23:38 +0900

    Create a basic singup page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "secret_key_here"
+
+users = {}
 
 @app.route("/")
 def home():
@@ -8,4 +11,26 @@ def home():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        dog_free = request.form.get("dog_free")
+
+        if username in users:
+            return "Username already exists!"
+
+        if dog_free != "on":
+            return "You must agree to the Dog-Free Oath."
+
+        users[username] = password
+        return redirect(url_for("login"))
+
+    return render_template("signup.html")
+
+@app.route("/login", methods=["GET", "POST"])
+
+
 
